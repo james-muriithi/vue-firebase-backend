4ef54db4411daa300b2e5720496fc1e280a9c1c3
James Muriithi <muriithijames556@gmail.com>
2021-03-23 23:42:31 +0300

    vue firebase backend

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,67 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No experiences found</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    };
+  },
+  mounted(){
+    this.loadExperiences()
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true
+      fetch('https://login-d4e7e.firebaseio.com/surveys.json')
+        .then(response => response.json())
+        .then(data => {
+          this.isLoading = false;
+          for(const id in data){
+            this.results.push({
+              id,
+              name: data[id].name,
+              rating: data[id].rating
+            })
+          }
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
